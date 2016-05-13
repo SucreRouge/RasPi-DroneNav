91708bc63dbd1f226dad0df4da8bbfae9bee64bf
MrOneTwo <ciesielskimm@gmail.com>
2016-05-13 19:49:48 +0200

    Initial commit.

---
+++ README.md
@@ -0,0 +1,7 @@
+# RasPi-OpenCV
+Different python scripts for Raspberry PI with OpenCV
+
+
+accessPiCam.py - take one shot from camera and display it.
+accessPiCamStream.py - stream camera view to screen.
+pi_camera_fps_demo.py - program showing the fps improvement with threads
---
+++ src/shapeDetectNav.py
@@ -0,0 +1,60 @@
+#!python2
+# -*- coding: UTF-8 -*-
+
+# REQUIREMENTS:
+#
+# pip install picamera[array]
+# pip install imutils
+#
+#
+
+# import the necessary packages
+from __future__ import print_function
+from imutils.video.pivideostream import PiVideoStream
+from imutils.video import FPS
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+import argparse
+import imutils
+import time
+import cv2
+
+
+# initialize the camera and stream
+camera = PiCamera()
+camera.resolution = (320, 240)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(320, 240))
+stream = camera.capture_continuous(rawCapture, format="bgr",
+                                   use_video_port=True)
+
+# created a *threaded *video stream, allow the camera sensor to warmup,
+# and start the FPS counter
+print("Starting threaded stream.")
+vs = PiVideoStream().start()
+time.sleep(2.0)
+fps = FPS().start()
+
+# loop over some frames...this time using the threaded stream
+while True:
+    # grab the frame from the threaded video stream and resize it
+    # to have a maximum width of 400 pixels
+    frame = vs.read()
+    frame = imutils.resize(frame, width=400)
+
+    # check to see if the frame should be displayed to our screen
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    # update the FPS counter
+    fps.update()
+
+    print("[INFO] elasped time: {:.2f}".format(fps.elapsed()))
+    print("[INFO] approx. FPS: {:.2f}".format(fps.fps()))
+
+# stop the timer and display FPS information
+fps.stop()
+
+# do a bit of cleanup
+cv2.destroyAllWindows()
+vs.stop()
