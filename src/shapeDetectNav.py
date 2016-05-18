a3056068f1de670ff8c603efcc09214519f47475
MrOneTwo <ciesielskimm@gmail.com>
2016-05-18 10:31:30 +0200

    Started adding settings dictionary. Toggling thresholded image with 'q' key.

--- src/shapeDetectNav.py
+++ src/shapeDetectNav.py
@@ -30,7 +30,7 @@ args = vars(ap.parse_args())
 
 # created a *threaded *video stream, allow the camera sensor to warmup,
 # and start the FPS counter
-print("Starting threaded stream.")
+print('Starting threaded stream.')
 vs = PiVideoStream().start()
 sd = ShapeDetector()
 time.sleep(2.0)
@@ -38,6 +38,8 @@ time.sleep(2.0)
 
 working = True
 i = 0
+settings = {'dispThresh': False, 'dispContours': True}
+# print(setting['dispThresh'])
 
 # loop over some frames...this time using the threaded stream
 while working:
@@ -46,16 +48,20 @@ while working:
     frame = vs.read()
     frame = imutils.resize(frame, width=600)
     frame = cv2.flip(frame, 0)
-    # frame = cv2.copyMakeBorder(frame, 3, 3, 3, 3, cv2.BORDER_CONSTANT, value = (255, 255, 255))
+    # frame = cv2.copyMakeBorder(frame, 3, 3, 3, 3,
+                                 # cv2.BORDER_CONSTANT, value=(255, 255, 255))
     frameGray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
     frameBlurred = cv2.GaussianBlur(frameGray, (5, 5), 0)
-    frameThresh = cv2.threshold(frameBlurred, 40, 255, cv2.THRESH_BINARY_INV)[1]
+    frameThresh = cv2.threshold(frameBlurred, 40, 255,
+                                cv2.THRESH_BINARY_INV)[1]
     # frameThresh = cv2.erode(frameThresh, None, iterations=1)
     # frameThresh = cv2.dilate(frameThresh, None, iterations=1)
-    frameThresh = cv2.copyMakeBorder(frameThresh, 3, 3, 3, 3, cv2.BORDER_CONSTANT, value = (0, 0, 0))
+    frameThresh = cv2.copyMakeBorder(frameThresh, 3, 3, 3, 3,
+                                     cv2.BORDER_CONSTANT, value=(0, 0, 0))
     frameFinal = frameThresh
 
-    cnts = cv2.findContours(frameFinal.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE) 
+    cnts = cv2.findContours(frameFinal.copy(),
+                            cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
     cnts = cnts[0] if imutils.is_cv2() else cnts[1]
     cntsCount = len(cnts)
     i = i + 1
@@ -73,18 +79,24 @@ while working:
         c = c.astype('int')
         cv2.drawContours(frame, [c], -1, (0, 255, 0), 1)
         cv2.putText(frame, shape, (cX, cY), cv2.FONT_HERSHEY_SIMPLEX,
-                0.5, (255, 255, 255), 1) # check to see if the frame should be displayed to our screen
+                    0.5, (255, 255, 255), 1)
 
-    for i in range(0, len(v)/2):
-        cv2.circle(frame, (v[2*i], v[2*i+1]), 4, (255, 100, 100), 1)
+    for i in range(0, len(v) / 2):
+        cv2.circle(frame, (v[2 * i], v[2 * i + 1]), 4, (255, 100, 100), 1)
 
+    # check to see if the frame should be displayed to our screen
+    if args['display'] > 0:
+        cv2.imshow('Frame', frame)
 
-    if args["display"] > 0:
-        cv2.imshow("Frame", frame)
+        if settings['dispThresh']:
+            cv2.imshow('Thresholded', frameFinal)
+
         key = cv2.waitKey(1) & 0xFF
 
     if key == 27:
         working = False
+    elif key == ord('q'):
+        settings['dispThresh'] = not settings['dispThresh']
 
     # update the FPS counter
     # fps.update()
