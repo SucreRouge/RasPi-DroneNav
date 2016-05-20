f33e535adf54ac290310e73d7f6d318bfa2f0911
MrOneTwo <ciesielskimm@gmail.com>
2016-05-20 18:50:11 +0200

    Changed framerate to 60.

--- src/piVideoStream/pivideostream.py
+++ src/piVideoStream/pivideostream.py
@@ -10,7 +10,7 @@ from threading import Thread
 
 
 class PiVideoStream:
-    def __init__(self, resolution=(320, 240), framerate=32):
+    def __init__(self, resolution=(320, 240), framerate=60):
         # initialize the camera and stream
         self.camera = PiCamera()
         self.camera.resolution = resolution
