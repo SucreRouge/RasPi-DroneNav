a3db93779e55d6bc0782fdcebad207eb8097157d
MrOneTwo <ciesielskimm@gmail.com>
2016-05-23 20:01:15 +0200

    Fixed lack of self... it's getting old.

--- src/CLInterface/CLInterface.py
+++ src/CLInterface/CLInterface.py
@@ -37,7 +37,7 @@ class CLInterface:
             waddstr(self.window, 'Drone navigation - vision based.\n')
             waddstr(self.window, '\n')
             waddstr(self.window, 'Parameters of the vision processing:\n')
-            waddstr(self.window, 'Threshold {0} - 255'.format(data['lowerThresh']))
+            waddstr(self.window, 'Threshold {0} - 255'.format(self.data['lowerThresh']))
 
             self.keyPressed = wgetch(self.window)
             if self.keyPressed == 27:
