11d8476abf439788ab61dd870f5766622cee62f6
MrOneTwo <ciesielskimm@gmail.com>
2016-05-23 20:19:53 +0200

    Working on passing the dictionary.

--- src/CLInterface/CLInterface.py
+++ src/CLInterface/CLInterface.py
@@ -44,7 +44,7 @@ class CLInterface:
                 self.running = False
         endwin()
 
-    def write(self, **dataIn):
+    def write(self, dataIn):
         self.data = dataIn
 
     def printData(self):
