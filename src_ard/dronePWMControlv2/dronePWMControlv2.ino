ba62034c98b5452e57f43525f10cef42874888f8
MrOneTwo <ciesielskimm@gmail.com>
2016-07-15 12:02:40 +0200

    Changed arduino code to 6 pins.

--- src_ard/dronePWMControlv2/dronePWMControlv2.ino
+++ src_ard/dronePWMControlv2/dronePWMControlv2.ino
@@ -9,8 +9,8 @@ bool On2 = false;
 bool On3 = false;
 bool On4 = false;
 bool On5 = false;
-bool On6 = false;
-bool On7 = false;
+// bool On6 = false;
+// bool On7 = false;
 
 
 uint16_t pwm0 = 103;
@@ -19,17 +19,17 @@ uint16_t pwm2 = 103;
 uint16_t pwm3 = 103;
 uint16_t pwm4 = 103;
 uint16_t pwm5 = 103;
-uint16_t pwm6 = 103;
-uint16_t pwm7 = 103;
+// uint16_t pwm6 = 103;
+// uint16_t pwm7 = 103;
 
 
 // 1  ms
-//a052b052c052d052e052f052g052h052i
+//a052b052c052d052e052f052g
 // 1.5ms
-//a078b078c078d078e078f078g078h078i
+//a078b078c078d078e078f078g
 // 2  ms
-//a103b103c103d103e103f103g103h103i
-//a999b999c999d999e999f999g999h999i
+//a103b103c103d103e103f103g
+//a999b999c999d999e999f999g
 void setup() {
     Serial.begin(115200);
 
@@ -41,7 +41,7 @@ void setup() {
     inputString.reserve(200);
     // PWM pins to output
     DDRC |= 0b00111111;
-    DDRD |= 0b01100000;
+    // DDRD |= 0b01100000;
 
     // synchronization info for RPi
     Serial.println("ARD - OK\n");
@@ -117,24 +117,24 @@ void loop() {
         PORTC &= ~_BV(PC5);
         On5 = false;
     }
- ////////////////////                                     6
-    if (m <= pwm6 && On6 == false){
-        PORTD |= _BV(PD5);
-        On6 = true;
-    }
-    if (m > pwm6 && On6 == true){
-        PORTD &= ~_BV(PD5);
-        On6 = false;
-    }
- ////////////////////                                     7
-    if (m <= pwm7 && On7 == false){
-        PORTD |= _BV(PD6);
-        On7 = true;
-    }
-    if (m > pwm7 && On7 == true){
-        PORTD &= ~_BV(PD6);
-        On7 = false;
-    }
+ // ////////////////////                                     6
+ //    if (m <= pwm6 && On6 == false){
+ //        PORTD |= _BV(PD5);
+ //        On6 = true;
+ //    }
+ //    if (m > pwm6 && On6 == true){
+ //        PORTD &= ~_BV(PD5);
+ //        On6 = false;
+ //    }
+ // ////////////////////                                     7
+ //    if (m <= pwm7 && On7 == false){
+ //        PORTD |= _BV(PD6);
+ //        On7 = true;
+ //    }
+ //    if (m > pwm7 && On7 == true){
+ //        PORTD &= ~_BV(PD6);
+ //        On7 = false;
+ //    }
 
 
     m = m + 1;
@@ -178,6 +178,6 @@ void serialEvent() {
     pwm3 = inputString.substring(inputString.indexOf('d')+1, inputString.indexOf('e')).toInt();
     pwm4 = inputString.substring(inputString.indexOf('e')+1, inputString.indexOf('f')).toInt();
     pwm5 = inputString.substring(inputString.indexOf('f')+1, inputString.indexOf('g')).toInt();
-    pwm6 = inputString.substring(inputString.indexOf('g')+1, inputString.indexOf('h')).toInt();
-    pwm7 = inputString.substring(inputString.indexOf('h')+1, inputString.indexOf('i')).toInt();
+    // pwm6 = inputString.substring(inputString.indexOf('g')+1, inputString.indexOf('h')).toInt();
+    // pwm7 = inputString.substring(inputString.indexOf('h')+1, inputString.indexOf('i')).toInt();
 }
